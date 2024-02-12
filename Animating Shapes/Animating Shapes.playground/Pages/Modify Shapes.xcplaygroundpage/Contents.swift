7661b9768b4b68db8ffd03587393ab6b77dfd613
7과11사이 <dongjunlee222@gmail.com>
2024-02-13 00:01:59 +0900

    [ADD]: ModifyShapes

---
+++ Animating Shapes/Animating Shapes.playground/Pages/Modify Shapes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+
+import SwiftUI
+
+struct ModifyShapes: View {
+    var body: some View {
+        VStack {
+            Capsule()
+//                .stroke(.green, lineWidth: 10)
+            // when using stroke, the edge of the outline can be clipped from the screen. The strokes are centered directly on the center of the shape's outline - which could lead to the clipping.
+
+            // use shapes that conform to InsettableShape - which creates strokes inside the shape.
+                .strokeBorder(.green, lineWidth: 10)
+            
+            Ellipse()
+                .strokeBorder(.red, style: StrokeStyle(lineWidth: 10, miterLimit: 10, dash: [20, 5], dashPhase: 5))
+            // the shape's color changes
+            Circle()
+                .foregroundColor(Color(red: 1.0, green: 0, blue: 1.0))
+            // the shape's color changes #2
+            Circle()
+                .fill(.indigo)
+            // only the background - excluding the color of shape
+            Circle()
+                .background(.indigo)
+            Capsule()
+                .strokeBorder(.indigo, lineWidth: 10)
+                .background(Capsule().fill(.pink))
+        }
+    }
+}
+
+struct ModifyShapes_Previews: PreviewProvider {
+    static var previews: some View {
+       ModifyShapes()
+    }
+}
+
+
